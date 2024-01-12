f944fd71f31838b4f74a8eb831117df5b65130f8
Ololade Otemade <adeniyiotemade@gmail.com>
2024-01-12 11:25:38 +0100

    Project setup

--- next.config.js
+++ next.config.js
@@ -2,3 +2,20 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+
+// // next.config.js
+// const withSass = require('@zeit/next-sass');
+
+// module.exports = withSass({
+//   /* additional configuration options here */
+// });
+
+// // next.config.js
+// const withSass = require('@zeit/next-sass');
+
+// const nextConfig = {
+//   /* other next.js configuration options can go here */
+// };
+
+// module.exports = withSass(nextConfig);
