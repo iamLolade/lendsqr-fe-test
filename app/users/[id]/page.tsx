0ac35edbe6cf1d6ca6f6d6dc75165687a8c584c7
Ololade Otemade <adeniyiotemade@gmail.com>
2024-01-16 23:43:57 +0100

    UI - Clean up

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,38 +1,43 @@
+'use client'
+
 import React from 'react'
 import Wrapper from '../../components/wrapper/Wrapper'
 import styles from './user.module.scss'
 import { HiOutlineArrowNarrowLeft } from "react-icons/hi";
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 
 const page = () => {
-  return (
-    <Wrapper>
-        <div className={styles.container}>
-            <div className={styles.return}>
-                <HiOutlineArrowNarrowLeft color='#545F7D' size={24} />
-                <p className={styles.text}>Back to Users</p>
-            </div>
-            <div className={styles.header}>
-                <p className={styles.title}>User Details</p>
-                <div className={styles.actions}>
-                    <button id={styles.blacklist}>
-                        Blacklist User
-                    </button>
-                    <button>
-                        Activate User
-                    </button>
-                </div>
-            </div>
+    const router = useRouter();
 
-            <div className={styles.summary}>
-                <div className={styles.details}>
-                    <Image src='/icons/avatar.svg' alt='icon' width={100} height={100} />
+    return (
+        <Wrapper>
+            <div className={styles.container}>
+                <div className={styles.return}>
+                    <HiOutlineArrowNarrowLeft color='#545F7D' size={24} onClick={() => router.push('/users')} className={styles.pointer} />
+                    <p className={styles.text}>Back to Users</p>
+                </div>
+                <div className={styles.header}>
+                    <p className={styles.title}>User Details</p>
+                    <div className={styles.actions}>
+                        <button id={styles.blacklist}>
+                            Blacklist User
+                        </button>
+                        <button>
+                            Activate User
+                        </button>
+                    </div>
+                </div>
+
+                <div className={styles.summary}>
+                    <div className={styles.details}>
+                        <Image src='/icons/avatar.svg' alt='icon' width={100} height={100} />
+                    </div>
                 </div>
             </div>
-        </div>
-    </Wrapper>
-  )
+        </Wrapper>
+    )
 }
 
 export default page
